b7197f2169ca8c5d0aff26df76f349d5007f0d89
quadrant26 <quadrant26@163.com>
2019-06-25 17:27:55 +0800

    actions

--- vuex-basic/src/vuex/store.js
+++ vuex-basic/src/vuex/store.js
@@ -22,8 +22,20 @@ const getters = {
     }
 }
 
+const actions = {
+    addAction (context){
+        context.commit('add', 10)
+        setTimeout(()=>{context.commit('reduce')}, 3000)
+        console.log("我比reduce先执行");
+    },
+    reduceAction ({commit}){
+        commit('reduce')
+    }
+}
+
 export default new Vuex.Store({
     state,
     mutations,
-    getters
+    getters,
+    actions
 })
