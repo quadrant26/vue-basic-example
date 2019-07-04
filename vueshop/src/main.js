dcd00ff7e41dbe4034193c32b668f9db63b85a0e
quadrant26 <quadrant26@163.com>
2019-07-04 16:26:30 +0800

    import babel-plugin-import 按需导入

--- vueshop/src/main.js
+++ vueshop/src/main.js
@@ -3,10 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vant from 'vant'
-import 'vant/lib/index.css'
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+import {Button} from 'vant'
 
-Vue.use(Vant)
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
