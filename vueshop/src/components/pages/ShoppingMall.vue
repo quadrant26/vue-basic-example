609207c88c8b352266707f00b4046a0faa2621d2
quadrant26 <quadrant26@163.com>
2019-07-04 17:31:02 +0800

    component ShoppingMall

---
+++ vueshop/src/components/pages/ShoppingMall.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="search-bar">
+            <van-row class="test-row">
+                <van-col span="3">
+                    <img :src="locationIcon" alt="定位" width="55%" class="location-icon">
+                </van-col>
+                <van-col span="16">
+                    <input type="text" class="search-input">
+                </van-col>
+                <van-col span="5">
+                    <van-button size="mini">搜索</van-button>
+                </van-col>
+            </van-row>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data (){
+        return {
+            msg: 'Shopping Mall',
+            locationIcon: require('../../assets/images/location.png')
+        }
+    }
+}
+</script>
+<style scoped>
+.search-bar{
+    height:2.2rem;
+    background-color: #e5017d;
+    line-height: 2.2rem;
+}
+.search-input{
+    width: 100%;
+    height:1.3rem;
+    border-width: 0 0 1px;
+    border-bottom:1px solid #fff;
+    background-color:#e5017d;
+    color:#fff;
+}
+.location-icon{
+    padding-top: 0.2rem;
+}
+</style>
