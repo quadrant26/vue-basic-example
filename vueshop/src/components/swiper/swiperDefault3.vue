a71eaaef6937e129b2547d31fc05580f72e942ad
quadrant26 <quadrant26@163.com>
2019-07-05 17:00:10 +0800

    how use vue-awesome-swiper

---
+++ vueshop/src/components/swiper/swiperDefault3.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <swiper class="swiper" :options="swiperOption">
+        <swiper-slide class="swiper-slide" v-for="(item, index) in slide" :key="index">
+            Slide {{item}}
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+</div>
+</template>
+
+<script>
+import {swiper, swiperSlide} from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+export default {
+    data (){
+        return {
+            slide: [1, 2, 3, 4, 5, 6],
+            swiperOption: {
+                direction: 'vertical',
+                pagination: {
+                    el: '.swiper-pagination'
+                }
+            }
+        }
+    },
+    components: {
+        swiper,
+        swiperSlide
+    }
+}
+</script>
+
+<style scoped>
+.swiper{ height:7rem; border-top:1px solid #f00; border-bottom: 1px solid #fff;}
+.swiper-slide{
+    height: 4rem;
+    text-align: center;
+    line-height: 4rem;
+}
+</style>
