29ae44c64cd0702832be1c115c90fa344c9f1bb2
quadrant26 <quadrant26@163.com>
2019-07-08 17:36:57 +0800

    异步使用 数据库 Promise

--- vueshop/service/database/init.js
+++ vueshop/service/database/init.js
@@ -4,21 +4,38 @@ const db = "mongodb://localhost/smile-db"
 exports.connect = ()=>{
     // 链接数据库
     mongoose.connect(db)
+    let maxConnectTimes = 0
 
-    // 添加数据库监听事件
-    mongoose.connection.on('disconnected', ()=>{
-        // 进行重连
-        console.log("*****数据库链接断开*****");
-        mongoose.connect(db);
-    })
+    return new Promise( (resolve, reject)=>{
+        // 添加数据库监听事件
+        mongoose.connection.on('disconnected', (err)=>{
+            // 进行重连
+            console.log("*****数据库链接断开*****");
+            if ( maxConnectTimes < 3){
+                maxConnectTimes ++;
+                mongoose.connect(db);
+            }else{
+                reject(err)
+                throw new Error('数据库出现问题，程序无法搞定，请认为修理.....')
+            }
+            
+        })
 
-    // 数据库出错
-    mongoose.connection.on('error', err=>{
-        console.log("*****数据库错误******")
-        mongoose.connect(db);
-    })
+        // 数据库出错
+        mongoose.connection.on('error', err=>{
+            console.log("*****数据库错误******")
+            if ( maxConnectTimes < 3){
+                maxConnectTimes ++;
+                mongoose.connect(db);
+            }else{
+                reject(err)
+                throw new Error('数据库出现问题，程序无法搞定，请认为修理.....')
+            }
+        })
 
-    mongoose.connection.once('open', ()=>{
-        console.log("[SUCCESS] MongoDB connected successfully!")
+        mongoose.connection.once('open', ()=>{
+            console.log("[SUCCESS] MongoDB connected successfully!")
+            resolve()
+        })
     })
 }
