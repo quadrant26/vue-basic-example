b0655404f5d69c959a38090b9383e131a93cd0bb
quadrant26 <quadrant26@163.com>
2019-07-08 17:29:17 +0800

    开启服务

--- vueshop/service/index.js
+++ vueshop/service/index.js
@@ -1,6 +1,13 @@
 const Koa = require('Koa')
 const app = new Koa()
 
+// 连接数据库
+const { connect } = require('./database/init.js')
+
+;(async ()=>{
+    await connect();
+})()
+
 app.use(async(ctx) => {
     ctx.body = '<h1>Hello Koa2</h1>'
 })
