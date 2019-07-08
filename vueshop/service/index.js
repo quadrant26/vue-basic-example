8b7444cf6fc633e7267b690d2b53e189752d56d6
quadrant26 <quadrant26@163.com>
2019-07-08 18:00:38 +0800

    schema 插入和查询数据库内容

--- vueshop/service/index.js
+++ vueshop/service/index.js
@@ -2,10 +2,23 @@ const Koa = require('Koa')
 const app = new Koa()
 
 // 连接数据库
-const { connect } = require('./database/init.js')
+const mongoose = require('mongoose')
+const { connect, initSchemas } = require('./database/init.js')
 
 ;(async ()=>{
     await connect();
+    // 载入 schema
+    initSchemas()
+    const User = mongoose.model('User')
+    // let oneUser = new User({userName: 'kang', password: '123456'})
+    // oneUser.save().then(()=>{
+    //     console.log('插入成功')
+    // })
+    
+    let users = await User.findOne({}).exec()
+    console.log('-----------------------')
+    console.log(users)
+    console.log('-----------------------')
 })()
 
 app.use(async(ctx) => {
