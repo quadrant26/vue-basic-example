ad1ceda6e4feb1b01c90afac604876b36e33179d
quadrant26 <quadrant26@163.com>
2019-07-08 17:29:09 +0800

    连接 mongodb 数据库

---
+++ vueshop/service/database/init.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const db = "mongodb://localhost/smile-db"
+
+exports.connect = ()=>{
+    // 链接数据库
+    mongoose.connect(db)
+
+    // 添加数据库监听事件
+    mongoose.connection.on('disconnected', ()=>{
+        // 进行重连
+        console.log("*****数据库链接断开*****");
+        mongoose.connect(db);
+    })
+
+    // 数据库出错
+    mongoose.connection.on('error', err=>{
+        console.log("*****数据库错误******")
+        mongoose.connect(db);
+    })
+
+    mongoose.connection.once('open', ()=>{
+        console.log("[SUCCESS] MongoDB connected successfully!")
+    })
+}
