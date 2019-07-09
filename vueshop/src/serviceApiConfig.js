151f1596b99dbb5b26d79dbe7e5723f9bdabc3cd
quadrant26 <quadrant26@163.com>
2019-07-09 17:57:35 +0800

    set LOCALURL

--- vueshop/src/serviceApiConfig.js
+++ vueshop/src/serviceApiConfig.js
@@ -1,7 +1,10 @@
 const BASEURL = 'https://www.easy-mock.com/mock/5d1dcefabaa6bf73722f7ec9/vueshop/'
+const LOCALURL = "http://localhost:3000/"
 const URL = {
     getShoppingMallInfo: BASEURL + 'index',
     getGoodsInfo: BASEURL + 'getGoodsInfo',
+    registerUser: LOCALURL+ 'user/register' // 用户注册接口
 }
+
 
 module.exports = URL;
