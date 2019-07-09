99bb18c7ded16acfd020f0dba3430a6aa41ede07
quadrant26 <quadrant26@163.com>
2019-07-09 17:56:30 +0800

    设置请求方式 body-parser和跨域 koa2-cors

--- vueshop/service/index.js
+++ vueshop/service/index.js
@@ -5,6 +5,14 @@ const app = new Koa()
 const mongoose = require('mongoose')
 const { connect, initSchemas } = require('./database/init.js')
 
+// 导入 koa-bodyParser
+const bodyParser = require('koa-bodyparser')
+app.use(bodyParser())
+
+// 配置跨域
+const cors = require('koa2-cors')
+app.use(cors())
+
 // 引入router
 const Router = require('koa-router')
 let user = require('./appApi/user.js')
