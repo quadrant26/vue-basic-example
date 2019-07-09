08ea1ccf43dc8832c3c6f3989b5d7f2ad42a7b08
quadrant26 <quadrant26@163.com>
2019-07-09 17:57:25 +0800

    /user/register 接收数据

--- vueshop/service/appApi/user.js
+++ vueshop/service/appApi/user.js
@@ -7,8 +7,10 @@ router.get('/', async(ctx) => {
     ctx.body = '用户操作首页'
 })
 
-router.get('/register', async(ctx) => {
-    ctx.body = '用户注册接口'
+router.post('/register', async(ctx) => {
+
+    console.log(ctx.request.body)
+    ctx.body = ctx.request.body
 })
 
 module.exports = router
