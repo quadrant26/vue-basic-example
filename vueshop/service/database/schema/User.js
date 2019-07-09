ff300b7efb24476d98ac619d943e4388027b98e0
quadrant26 <quadrant26@163.com>
2019-07-09 16:54:59 +0800

    bcrypt 密码加盐

--- vueshop/service/database/schema/User.js
+++ vueshop/service/database/schema/User.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10;
+
 let ObjectId = Schema.Types.ObjectId
 
 // 创建UserSchema
@@ -10,6 +13,18 @@ const userSchema = new Schema({
     password: String,
     createAt: {type: Date, default: Date.now()},
     lastLoginAt: {type: Date, default: Date.now()}
+})
+
+userSchema.pre('save', function (next){
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err)return next(err)
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if(err)return next(err)
+            this.password = hash
+            next();
+        })
+
+    })
 })
 
 // 发布
--- vueshop/service/index.js
+++ vueshop/service/index.js
@@ -10,15 +10,15 @@ const { connect, initSchemas } = require('./database/init.js')
     // 载入 schema
     initSchemas()
     const User = mongoose.model('User')
-    // let oneUser = new User({userName: 'kang', password: '123456'})
-    // oneUser.save().then(()=>{
-    //     console.log('插入成功')
-    // })
+    let oneUser = new User({userName: 'kang2', password: '123456'})
+    oneUser.save().then(()=>{
+        console.log('插入成功')
+    })
     
-    let users = await User.findOne({}).exec()
-    console.log('-----------------------')
-    console.log(users)
-    console.log('-----------------------')
+    // let users = await User.findOne({}).exec()
+    // console.log('-----------------------')
+    // console.log(users)
+    // console.log('-----------------------')
 })()
 
 app.use(async(ctx) => {
