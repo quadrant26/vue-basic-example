ea738236766b4457fed1adad08f6aa156a032228
quadrant26 <quadrant26@163.com>
2019-07-09 18:09:15 +0800

    注册接受数据处理

--- vueshop/service/index.js
+++ vueshop/service/index.js
@@ -27,21 +27,21 @@ router.use('/home', home.routes())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-// ;(async ()=>{
-//     await connect();
-//     // 载入 schema
-//     initSchemas()
-//     const User = mongoose.model('User')
-//     let oneUser = new User({userName: 'kang2', password: '123456'})
-//     oneUser.save().then(()=>{
-//         console.log('插入成功')
-//     })
+;(async ()=>{
+    await connect();
+    // 载入 schema
+    initSchemas()
+    // const User = mongoose.model('User')
+    // let oneUser = new User({userName: 'kang2', password: '123456'})
+    // oneUser.save().then(()=>{
+    //     console.log('插入成功')
+    // })
     
-//     // let users = await User.findOne({}).exec()
-//     // console.log('-----------------------')
-//     // console.log(users)
-//     // console.log('-----------------------')
-// })()
+    // let users = await User.findOne({}).exec()
+    // console.log('-----------------------')
+    // console.log(users)
+    // console.log('-----------------------')
+})()
 
 app.use(async(ctx) => {
     ctx.body = '<h1>Hello Koa2</h1>'
