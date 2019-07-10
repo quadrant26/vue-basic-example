3f1ac76eaa4a63d0035ea864a20985f7025d25e4
quadrant26 <quadrant26@163.com>
2019-07-10 15:31:12 +0800

    添加 login 路由

--- vueshop/src/router/index.js
+++ vueshop/src/router/index.js
@@ -3,12 +3,14 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import ShoppingMall from '@/components/pages/ShoppingMall'
 import Register from '@/components/pages/Register'
+import Login from '@/components/pages/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path: '/', name: 'ShoppingMall', component: ShoppingMall },
-    {path: '/register', name: 'register', component: Register}
+    {path: '/register', name: 'register', component: Register},
+    {path: '/login', name: 'login', component: Login}
   ]
 })
