3d2dd2bd037f24c09a3e66d4ea6e9b9541c7c7b3
quadrant26 <quadrant26@163.com>
2019-07-10 17:18:18 +0800

    后台添加 获取商品详细信息接口 getDetailGoodsInfo

--- vueshop/service/appApi/goods.js
+++ vueshop/service/appApi/goods.js
@@ -64,4 +64,16 @@ router.get('/insertAllCategorySub', async( ctx)=>{
     ctx.body = "开始插入 category-sub 数据...."
 })
 
+// 获取商品详情信息接口
+router.post('/getDetailGoodsInfo', async (ctx) => {
+    let goodsId = ctx.request.body.goodsId
+    const Goods = mongoose.model('Goods')
+    await Goods.findOne({ID: goodsId}).exec().then( async (result) => {
+        ctx.body = { code: 200, message: result}
+    }).catch( error => {
+        console.log( error)
+        ctx.body = { code: 500, message: error}
+    })
+})
+
 module.exports = router
