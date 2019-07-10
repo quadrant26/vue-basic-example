87c0175433f53ed4d5418dac95bb7db84df62690
quadrant26 <quadrant26@163.com>
2019-07-10 15:32:11 +0800

    处理 login 接口和数据比对

--- vueshop/service/appApi/user.js
+++ vueshop/service/appApi/user.js
@@ -27,4 +27,33 @@ router.post('/register', async(ctx) => {
     })
 })
 
+router.post('/login', async(ctx) => {
+
+    let loginUser = ctx.request.body
+    console.log(loginUser)
+    let userName = loginUser.userName
+    let password = loginUser.password
+
+    // 进行数据库查找
+    const User = mongoose.model('User')
+    await User.findOne({userName: userName}).exec().then( async(result) => {
+        console.log(result)
+        if ( result ){
+            // 结果存在，去比对密码
+            let newUser = new User()
+            await newUser.comparePassword(password, result.password)
+            .then(isMatch => {
+                ctx.body = {code: 200, message: isMatch}
+            }).catch( error => {
+                console.log(error)
+                ctx.body = { code: 200, message: error}
+            })
+        }else{
+            ctx.body = { code: 200, message: '用户名不存在'}
+        }
+    }).catch( (error) => {
+        ctx.body = {code: 500, message: error}
+    })
+})
+
 module.exports = router
