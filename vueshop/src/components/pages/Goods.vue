5d4c9f261f65d1840a260f05b931307f1a30a2c4
quadrant26 <quadrant26@163.com>
2019-07-10 17:19:18 +0800

    component goods

---
+++ vueshop/src/components/pages/Goods.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        商品详情
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import url from '@/serviceApiConfig.js'
+export default {
+    data (){
+        return {
+            goodsId: '00b417a0f3444bb6965f4ba2dbafbed0'
+        }
+    },
+    created (){
+        this.getInfo()
+    },
+    methods: {
+        getInfo (){
+            axios({
+                url: url.getDetailGoodsInfo,
+                method: 'post',
+                data: {
+                    goodsId: this.goodsId
+                }
+            }).then( response => {
+                console.log( response)
+            }).catch( error => {
+                console.log( error)
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
