24273b55a52fbcdae7de156ad29e4a5b6c18fa3c
quadrant26 <quadrant26@163.com>
2019-07-10 15:31:33 +0800

    添加 login 接口

--- vueshop/src/serviceApiConfig.js
+++ vueshop/src/serviceApiConfig.js
@@ -3,7 +3,8 @@ const LOCALURL = "http://localhost:3000/"
 const URL = {
     getShoppingMallInfo: BASEURL + 'index',
     getGoodsInfo: BASEURL + 'getGoodsInfo',
-    registerUser: LOCALURL+ 'user/register' // 用户注册接口
+    registerUser: LOCALURL+ 'user/register', // 用户注册接口
+    login: LOCALURL+ 'user/login', // 用户登录接口
 }
 
 
