3db6c3c2ed0b73d45dc818597620da5db31f6870
quadrant26 <quadrant26@163.com>
2019-07-11 15:00:55 +0800

    goods 详情页

--- vueshop/src/components/pages/Goods.vue
+++ vueshop/src/components/pages/Goods.vue
@@ -1,17 +1,53 @@
 <template>
     <div>
-        商品详情
+        <div class="navbar-div">
+            <van-nav-bar
+                title='商品详情'
+                left-text="返回"
+                left-arrow
+                @click-left="onClickLeft"
+            ></van-nav-bar>
+        </div>
+        <!--begin: 商品头图 -->
+        <div class="topimage-div">
+            <img :src="goodsInfo.IMAGE1" width="100%" alt="">
+        </div>
+        <div class="goods-name">{{goodsInfo.NAME}}</div>
+        <div class="goods-price">价格：{{goodsInfo.PRESENT_PRICE | moneyFilter}}</div>
+        <div>
+            <van-tabs swipeable sticky>
+                <van-tab title="商品详情">
+                    <div class="detail" v-html="goodsInfo.DETAIL">
+                        
+                    </div>
+                </van-tab>
+                <van-tab title="评价">
+                    正在制作中
+                </van-tab>
+            </van-tabs>
+        </div>
+        <div class="goods-bottom">
+            <div>
+                <van-button size="large" type="primary">加入购物车</van-button>
+            </div>
+            <div>
+                <van-button size="large" type="danger">直接购买</van-button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
 import url from '@/serviceApiConfig.js'
+import { Toast } from 'vant'
+import {toMoney} from '@/filter/moneyFilter.js'
 export default {
     props: [],
     data (){
         return {
-            goodsId: ''
+            goodsId: '',
+            goodsInfo: {}, // 商品详细信息
         }
     },
     created (){
@@ -27,15 +63,52 @@ export default {
                     goodsId: this.goodsId
                 }
             }).then( response => {
-                console.log( response)
+                
+                if ( response.data.code == 200 && response.data.message){
+                    this.goodsInfo = response.data.message
+                }else{
+                    Toast('服务器错误，数据获取失败')
+                }
+                
             }).catch( error => {
                 console.log( error)
             })
+        },
+        onClickLeft (){
+            this.$router.go(-1)
+        }
+    },
+    filters: {
+        moneyFilter (money){
+            return toMoney(money)
         }
     }
 }
 </script>
 
 <style scoped>
+.detail{
+    font-size:0px;
+}
+.goods-name{
+    background-color: #fff;
+}
+.goods-price{
+    background-color: #fff;
+}
+.goods-bottom{
+    position: fixed;
+    bottom:0px;
+    left:0px;
+    background-color: #FFF;
+    width:100%;
 
+    display: flex;
+    flex-direction: row;
+    flex-flow: nowrap;
+}
+.goods-bottom > div{
+    flex:1;
+    padding:5px;
+}
 </style>
