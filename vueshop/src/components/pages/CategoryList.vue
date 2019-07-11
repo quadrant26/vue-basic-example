12583ad6790da783258470e1ad3964d73927a36e
quadrant26 <quadrant26@163.com>
2019-07-11 15:41:26 +0800

    左侧分类内容设置

--- vueshop/src/components/pages/CategoryList.vue
+++ vueshop/src/components/pages/CategoryList.vue
@@ -8,7 +8,14 @@
         <div>
             <van-row>
                 <van-col span="6">
-                    <div id="leftNav">左侧</div>
+                    <div id="leftNav">
+                        <ul>
+                            <li v-for="(item, index) in category" :key="index" @click="clickCategory(index)"
+                            :class="{categoryActive:categoryIndex==index}">
+                                {{item.MALL_CATEGORY_NAME}}
+                            </li>
+                        </ul>
+                    </div>
                 </van-col>
                 <van-col span="18">
                     右侧
@@ -26,11 +33,16 @@ import { Toast } from 'vant'
 export default {
     data (){
         return {
-            message: []
+            category: [],
+            categoryIndex: 0
         }
     },
     created (){
         this.getCategory()
+    },
+    mounted (){
+        let winHeight = document.documentElement.clientHeight
+        document.getElementById("leftNav").style.height= winHeight-46 +'px'
     },
     methods: {
         getCategory (){
@@ -40,12 +52,16 @@ export default {
             }).then( response => {
                 if ( response.data.code == 200 && response.data.message){
                     console.log(response.data.message);
+                    this.category = response.data.message
                 }else{
                     Toast('服务器错误，数据获取错误')
                 }
             }).catch(error => {
                 console.log(error)
             })
+        },
+        clickCategory(index){
+            this.categoryIndex=index
         }
     }
 }
@@ -54,5 +70,15 @@ export default {
 <style scoped>
 #leftNav{
     background-color: aliceblue;
+}
+#leftNav ul li {
+    line-height: 2rem;
+    border-bottom:1px solid #E4E7ED;
+    padding:3px;
+    font-size:0.8rem;
+    text-align: center;
+}
+.categoryActive{
+    background-color: #fff;
 }
 </style>
