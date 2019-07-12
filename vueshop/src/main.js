48edb1b831ca33f1ebfd8f1500a38b087f15eb7d
quadrant26 <quadrant26@163.com>
2019-07-12 16:13:02 +0800

    导入vant Tabbar 和 TabbarItem 组件

--- vueshop/src/main.js
+++ vueshop/src/main.js
@@ -6,12 +6,12 @@ import router from './router'
 import VueLazyload from 'vue-lazyload'  //引入这个懒加载插件
 // import Vant from 'vant'
 // import 'vant/lib/index.css'
-import {Button, Row, Col, Swipe, SwipeItem, List, Field, NavBar, Tab, Tabs, PullRefresh, Stepper} from 'vant'
+import {Tabbar, TabbarItem, Button, Row, Col, Swipe, SwipeItem, List, Field, NavBar, Tab, Tabs, PullRefresh, Stepper} from 'vant'
 // import VueAwesomeSwiper from 'vue-awesome-swiper'
 // import 'swiper/dist/css/swiper.css'
 
 Vue.use(VueLazyload)
-Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(List).use(Field).use(NavBar).use(Tab).use(Tabs).use(PullRefresh).use(Stepper)
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(List).use(Field).use(NavBar).use(Tab).use(Tabs).use(PullRefresh).use(Stepper).use(Tabbar).use(TabbarItem)
 // Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
